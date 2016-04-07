62168a00fdb93feb229b0084d65816763efc899a
Ivan Baranov <baranov.vania@gmail.com>
2016-04-07 12:44:31 +0300

    Add JSCS validation

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,26 @@
 var gulp = require('gulp');
 var wrap = require('gulp-wrap');
 var handlebars = require('gulp-handlebars');
+var jscs = require('gulp-jscs');
+var stylish = require('gulp-jscs-stylish');
 
 var buildTpl = function() {
     return wrap("module.exports = require('handlebars').template(<%= contents %>)");
-}
+};
+
+gulp.task('default', ['build', 'jscs']);
  
 gulp.task('build', function() {
-    gulp.src('src/templates/**/*.hbs')
+    return gulp.src('src/templates/**/*.hbs')
         .pipe(handlebars({
             handlebars: require('handlebars')
         }))
         .pipe(buildTpl())
         .pipe(gulp.dest('src/templates/build/'));
+});
+
+gulp.task('jscs', function() {
+    return gulp.src('src/**/*.js')
+        .pipe(jscs())
+        .pipe(stylish());
 });
