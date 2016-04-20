f16ced438384d17d091f65caf1d662af626a3468
designeng <denissavenok@gmail.com>
2016-04-20 13:53:30 +0300

    comment

--- src/routes.js
+++ src/routes.js
@@ -63,6 +63,9 @@ const routes = [
             url: '/brands/:brand/models/:year/:model',
             routeSpec: brandModelPageSpec
       },
+
+      // left for backwards compatibility with previous site version, 
+      // redirect to '/brands/:brand/models/:year/:model'
       {
             url: '/:brand/models/:year/:model',
             routeSpec: brandModelPageSpec
