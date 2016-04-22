8969972ce87e98b00daa5c5ae8bd1177ee68562c
designeng <denissavenok@gmail.com>
2016-04-22 11:35:22 +0300

    tracing; originUrl fixed

--- src/plugins/express/proxy/middleware.js
+++ src/plugins/express/proxy/middleware.js
@@ -9,20 +9,23 @@ function proxyMiddleware(resolver, facet, wire) {
         let method = route.method.toLowerCase() || 'get';
 
         target[method](route.url, function (req, res) {
-            let query = url.parse(req.url, true).query;
-            let originUrl = route.originUrl;
-            originUrl.slice(-1) != '/' ? originUrl += '/' : void 0;
-            let restParams = req.params;
-            if(restParams) {
-                for(let key in restParams) {
-                    originUrl += restParams[key] + '/'
-                }
-            }
+            // let query = url.parse(req.url, true).query;
+            // let originUrl = route.originUrl;
+            // originUrl.slice(-1) != '/' ? originUrl += '/' : void 0;
+            // let restParams = req.params;
+            // if(restParams) {
+            //     for(let key in restParams) {
+            //         originUrl += restParams[key] + '/'
+            //     }
+            // }
 
             console.log('req.body.email:::', req.body.email);
+            console.log('req.body.text:::', req.body.text);
+            console.log('req.body.mode:::', req.body.mode);
+            console.log('req.body.url:::', req.body.url);
 
             // axios[method](originUrl, { params: query })
-            axios[method](originUrl, {
+            axios[method](route.originUrl, {
                     email:  req.body.email,
                     text:   req.body.text,
                     mode:   req.body.mode,
