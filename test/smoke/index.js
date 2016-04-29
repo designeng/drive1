d3db79a5be7b8d3fd830a4fe9488c3b8dc1c2d1c
designeng <denissavenok@gmail.com>
2016-04-29 18:33:41 +0300

    cleaned code

--- test/smoke/index.js
+++ test/smoke/index.js
@@ -8,10 +8,8 @@ import receptionSpec         from '../../src/reception.spec';
 import { createTasks, createTask } from '../../src/utils/tasks';
 
 let environment = {
-    // accessToken: '57rvBRYOy1ztDOFUM0PRfipzIipfqf9tVjFuvsWkixMPsFtmqYjOv7mhsjtxD6OdJRyooF1C-5bjE4zOk7MWgXxJUDtTI7YXgjHCtaZ4jk64rme0dy588bfahGOddERZ',
-    accessToken: 't3h8_FsEh8Nqcm8P7NhtePmo3Uqr9isqm0nbIEEwFk4AAH5PL4d0VNGANFGX_jKj7MoBqHsQZ3F1DrMhHsz9YkKQc9kHLGvXxRiC7Ax-xUUZg_jru45hN6z6ONJmdE8T',
-    targetUrl: 'ttp://localhost:3002/auth',
-    response: {}
+    accessToken: '',
+    targetUrl: 'ttp://localhost:3002/auth'
 }
 
 const tasks = createTasks([environment, receptionSpec]);
