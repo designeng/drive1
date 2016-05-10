12f9c3e5b50fba70b868d4b7472e0d18cb71c95c
designeng <denissavenok@gmail.com>
2016-05-10 16:44:45 +0300

    data formatted

--- src/utils/preprocessCaption.js
+++ src/utils/preprocessCaption.js
@@ -1,13 +1,13 @@
 import _ from 'underscore';
 import caption from './caption';
 
-// import moment from 'moment';
-// moment.locale('ru');
+import moment from 'moment';
+moment.locale('ru');
 
 export default function preprocessCaption(items, mode) {
     return _.map(items, (item) => {
         return _.extend({}, item, {
-            // time    : moment.unix(item.time).fromNow(),
+            time    : moment.unix(item.time).format('D MMMM YYYY'),
             caption : caption(item, mode)
         });
     });
